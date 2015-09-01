aa41799c18a564802a8a0f4c389270dacd2132dd
Reza Fatahi <aug2uag@gmail.com>
2015-09-01 03:05:38 -0700

    post.sh

--- post.sh
+++ post.sh
@@ -5,14 +5,16 @@
  if [ "$#" -eq  "0" ] then
  	{
  		curl -H "Content-Type: application/json" --data @json.json http://localhost:8080/
+ 		file="json.json"
+		[[ -f "$file" ]] && rm -f "$file"
 	} || {
-	    echo "failed without argument"
+	    echo "failed without argument, json file must be named \"json.json\""
 	}
  else
 	{
 		curl -H "Content-type: application/json" -X POST -d "$1" http://localhost:8080/
 
 	} || {
-	    echo "failed with argument"
+	    echo "failed with argument, please verify json at http://jsonlint.com and try again"
 	}
  fi
