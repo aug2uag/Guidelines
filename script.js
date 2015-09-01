583424d19ba508ccd456aa0466215e33bc474525
Reza Fatahi <aug2uag@gmail.com>
2015-09-01 11:57:09 -0700

    lib helper and guidelines have their own dir

--- script.js
+++ script.js
@@ -6,20 +6,23 @@ var server_port = 8080
   , express	 = require('express')
   , app 	 = express()
   , bParse   = require('body-parser')
+  , fs 		 = require('fs');
 
-app.use(bParse.json())
+app.use(bParse.json());
 
 app.post('/', function(req, res) {
 	var json2csv = require('json2csv')
 	  , json2xml = require('json2xml')
 	  , document = req.body
 	  , guidedir = document.type
-	  , jsonFile = guidedir+'/'+guidedir+'_guidelines.json'
-	  , _xmlFile = guidedir+'/'+guidedir+'_guidelines.xml'
-	  , _csvFile = guidedir+'/'+guidedir+'_guidelines.csv'
+	  , jsonFile = './guidelines/'+guidedir+'/'+guidedir+'_guidelines.json'
+	  , _xmlFile = './guidelines/'+guidedir+'/'+guidedir+'_guidelines.xml'
+	  , _csvFile = './guidelines/'+guidedir+'/'+guidedir+'_guidelines.csv'
 	  , json = document.json
-	  , xml = json2xml(json);
+	  , xml = json2xml(json)
+	  , dir = './guidelines/'+guidedir;
 	if (!guidedir || !json) return res.send(400, 'json not formatted');
+	if (!fs.existsSync(dir))fs.mkdirSync(dir);
 	fs.writeFile(jsonFile, json, function(err) {
 		if (err) throw err;
 		fs.writeFile(_xmlFile, xml, function (err) {
@@ -27,7 +30,7 @@ app.post('/', function(req, res) {
 			json2csv(json, function(err, csv) {
 				fs.writeFile(_csvFile, csv, function (err) {
 					if (err) throw err;
-					res.json({v: 'Guidelines v1.0.0'})
+					res.json({v: 'Guidelines v1.0.0'});
 				});
 			});
 		});
--- server.js
+++ server.js
@@ -5,9 +5,7 @@
 var express	 = require('express')
   , app 	 = express()
   , bParse   = require('body-parser')
-  , fs = require('fs')
-  , path = require('path')
-  , self = this;
+  , Utils 	 = require('./lib/utils');
 
 app.use(bParse.json())
 
@@ -16,36 +14,18 @@ app.get('/', function(req, res) {
 });
 
 app.get('/disciplines', function(req, res) {
-	fs.readdir('./', function (err, files) {
-	    if (err) throw err;
-	    var acc = files.map(function (file) {
-	        return path.join('./', file);
-	    }).filter(function (file) {
-	        return fs.statSync(file).isDirectory();
-	    });
-	    var agg=[];
-	    function final() {res.json(agg)}
-		function series(dir) {
-			if(dir) {
-				if (dir!=='.git'&&dir!=='node_modules') agg.push(dir);
-				return series(acc.shift());
-		  } else {
-			return final();
-		  }
-		}
-		series(acc.shift());
+	Utils.getDisciplines(function(err, result) {
+		if (err) throw err;
+		else res.json(result);
 	});
 });
 
 app.post('/get', function(req, res) {
 	var document = req.body
 	  , discipline = document.discipline;
-	console.log(discipline)
-	if (!fs.statSync(discipline).isDirectory()) return res.send(400, 'not valid discipline');
-	discipline = './' + discipline + '/' + discipline + '_guidelines.json';
-	fs.readFile(discipline, 'utf8', function(err, contents) {
-	    contents = JSON.parse(contents);
-	    res.json(Object.keys(contents));
+	Utils.getDiscipline(discipline, function(err, result) {
+		if (err) throw err;
+		else res.json(Object.keys(result));
 	});
 });
 
@@ -53,30 +33,16 @@ app.post('/doc', function(req, res) {
 	var document = req.body
 	  , discipline = document.discipline
 	  , _document = document.document;
-	if (!fs.statSync(discipline).isDirectory()) return res.send(400, 'not valid discipline');
-	discipline = './' + discipline + '/' + discipline + '_guidelines.json';
-	fs.readFile(discipline, 'utf8', function(err, contents) {
-	    contents = JSON.parse(contents);
-	    var url = contents[_document];
-	    http.get(url, function(response) {
-			var chunks = [];
-			response.on('data', function(chunk) {
-				chunks.push(chunk);
-			});
-			response.on("end", function() {
-				var pdf = new Buffer.concat(chunks).toString('base64');
-				res.header('Content-Type', 'application/pdf');
-				res.send(pdf);
-			});
-		}).on("error", function(err) {
-			throw err;
-		});
-	});
+	Utils.getPDF(discipline, _document, function(err, result) {
+		if (err) throw err;
+		res.header('Content-Type', 'application/pdf');
+		res.send(result);
+	})
 });
 
-self.ipaddr  = process.env.OPENSHIFT_NODEJS_IP || 'localhost';
-self.port    = process.env.OPENSHIFT_NODEJS_PORT || 9999;
+var _ipaddr  = process.env.OPENSHIFT_NODEJS_IP || 'localhost';
+var _port    = process.env.OPENSHIFT_NODEJS_PORT || 9999;
 
-app.listen(self.port, self.ipaddr, function() {
+app.listen(_port, _ipaddr, function() {
   console.log('booted, all systems go  || ' + new Date() + '\n' + Array(40).join('* '))
 });
