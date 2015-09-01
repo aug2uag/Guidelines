a313eb90fbf3dea693d5c12ad8548b3448fe9232
Reza Fatahi <aug2uag@gmail.com>
2015-09-01 03:27:24 -0700

    post.sh args should be same

--- post.sh
+++ post.sh
@@ -4,7 +4,7 @@
 
  if [ "$#" -eq  "0" ] then
 	file=($(ls *.json))
-	curl -H "Content-Type: application/json" --data @"$file" http://localhost:8080/
+	curl -H "Content-Type: application/json" -d @"$file" http://localhost:8080/
 	rm -f "$file"
  else
 	curl -H "Content-type: application/json" -X POST -d "$1" http://localhost:8080/
